9ae6b6f5706fa57a8b970d07fd572fe24472bbe8
dylanabzr <dylanab@pm.me>
2023-09-29 19:58:34 -0300

    Just some style updates

--- src/movement.cpp
+++ src/movement.cpp
@@ -12,7 +12,7 @@ class Game_Objects{
   protected:
     Object human = {WINDOW_WIDTH/2, 3 * WINDOW_HEIGHT / 5, 50, 30};
     Object ground = {0, human.posY + human.height, 5, WINDOW_WIDTH};
-    bool isColliding(const Object obj1, const Object obj2) {
+    bool isColliding(const Object obj1, const Object obj2) { // for now that function is useless, but i'll use it in the future :)
       bool xOverlap = (obj1.posX < obj2.posX + obj2.width) && (obj2.posX < obj1.posX + obj1.width);
       bool yOverlap =  (obj1.posY < obj2.posY + obj2.height) && (obj2.posY < obj1.posY + obj1.height);
       return (xOverlap && yOverlap);
@@ -25,19 +25,19 @@ class Movement : public Game_Objects{
     char X_Movement_Status = 0, Y_Movement_Status = 0;
     int Xcount = 0, Ycount = 0;
     void walk(){
-      if(IsKeyDown(KEY_D)) X_Movement_Status = 1;
-      if(IsKeyDown(KEY_A)) X_Movement_Status = -1;
+      if (IsKeyDown(KEY_D)) X_Movement_Status = 1;
+      if (IsKeyDown(KEY_A)) X_Movement_Status = -1;
       human.posX += 5 * X_Movement_Status;
-      if(X_Movement_Status) Xcount++;
-      if(Xcount >= 10) {
+      if (X_Movement_Status) Xcount++;
+      if (Xcount >= 10) {
         X_Movement_Status = 0;
         Xcount = 0;
       } 
     }
     void jump(){
-      if(!Y_Movement_Status){
-        if(IsKeyDown(KEY_SPACE)) Y_Movement_Status = 1;
-      }else{
+      if (!Y_Movement_Status) {
+        if (IsKeyDown(KEY_SPACE)) Y_Movement_Status = 1;
+      } else {
         human.posY -= 7 * Y_Movement_Status;
         Ycount++;
         if (Y_Movement_Status == 1){
@@ -46,8 +46,8 @@ class Movement : public Game_Objects{
             Ycount=0;
           }
         }
-        if(Y_Movement_Status == -1){
-            if(Ycount >= 15){
+        if (Y_Movement_Status == -1) {
+            if (Ycount >= 15) {
               Y_Movement_Status = 0;
               Ycount = 0;
             }
@@ -57,7 +57,7 @@ class Movement : public Game_Objects{
     void basicMovement(){
       walk();
       jump();
-  }
+    }
     
 };
 
@@ -75,7 +75,7 @@ class Graphics : Movement{
 
 class Window : Graphics{
   public:
-    void windowLoop(){
+    void windowLoop() {
       InitWindow(WINDOW_HEIGHT, WINDOW_WIDTH, "Physics");
       SetTargetFPS(60);
       while(!WindowShouldClose()){
@@ -86,12 +86,12 @@ class Window : Graphics{
     }
 };
 
-void startGame(){
+void startGame() {
   Window window;
   window.windowLoop();
 }
 
-int main(){
+int main() {
   startGame();
   return 0; 
 }
